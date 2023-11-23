d2472d3c6a2940e13ad66082dbe5152e7db644c9
Pedro Brito <phvdbrito@gmail.com>
2023-11-23 09:06:00 -0300

    atualizacao codg

--- main.py
+++ main.py
@@ -1,106 +1,65 @@
-from flask import Flask, request, jsonify, send_file
+from flask import Flask, request, jsonify
 import pydicom
 import os
-from pathlib import Path
-from werkzeug.utils import secure_filename
+import SimpleITK as sitk
 import numpy as np
 
 app = Flask(__name__)
 
-def segment_image(dicom_file, segmentation_parameters):
-    try:
-        # Caminho do diretório para salvar os arquivos processados
-        processed_dir = os.path.join(os.getcwd(), 'processed')
-        Path(processed_dir).mkdir(parents=True, exist_ok=True)
+def anonimize_dicom(dicom_file):
+    ds = pydicom.dcmread(dicom_file)
 
-        # Leitura do arquivo DICOM
-        dicom_dataset = pydicom.dcmread(dicom_file)
+    # Anonimizar os metadados desejados
 
-        # Anonimização dos metadados
-        anonymize_dicom(dicom_dataset)
+    ds.PatientName = "Metadado Anonimizado pela API"
+    ds.PatientSex = "Metadado Anonimizado pela API"
+    ds.PatientBirthDate = "000000"
+    ds.PatientID = "000000"
+    ds.PatientAge = "00"
+    ds.PatientSize = "00"
+    ds.PatientWeight = "00"
 
-        # Obtenha os dados da imagem como numpy array
-        image_array = dicom_dataset.pixel_array
+    return ds
 
-        # Implementação genérica de segmentação usando o método de limiarização
-        lower_threshold = segmentation_parameters.get('lower_threshold', 0)
-        upper_threshold = segmentation_parameters.get('upper_threshold', 255)
-        segmented_array = np.where((image_array >= lower_threshold) & (image_array <= upper_threshold), 1, 0)
+def segment_dicom_image(dicom_file):
+    ds = pydicom.dcmread(dicom_file)
 
-        # Nome do novo arquivo DICOM com "processed"
-        base_name = secure_filename(dicom_file.filename).split('.')[0]
-        processed_dicom_path = os.path.join(processed_dir, f'processed_{base_name}.dcm')
+    # Obter matriz de pixel
+    pixel_array = ds.pixel_array
 
-        # Salve o DICOM
-        save_dicom(segmented_array, dicom_dataset, processed_dicom_path)
+    # Converter para imagem SimpleITK
+    image = sitk.GetImageFromArray(pixel_array)
 
-        return processed_dicom_path
+    # Segmentação usando SimpleITK (exemplo simples - ajuste conforme necessário)
+    segmentation = sitk.BinaryThreshold(image, lowerThreshold=100, upperThreshold=200)
 
-    except Exception as e:
-        raise e
+    # Converter imagem segmentada para numpy array
+    segmented_array = sitk.GetArrayFromImage(segmentation)
 
-def anonymize_dicom(dicom_dataset):
-    # Anonimização dos metadados conforme necessário
-    dicom_dataset.PatientName = "Anonymous"
-    dicom_dataset.PatientID = "AnonID"
-    dicom_dataset.PatientBirthDate = "19000101"
-    dicom_dataset.InstitutionName = "Anonymous Institution"
-    dicom_dataset.InstitutionAddress = "Anonymous Address"
-    # Adicione mais campos conforme necessário
-
-def save_dicom(pixel_array, original_dicom, save_path):
-    # Crie um novo objeto DICOM para o arquivo atualizado
-    updated_dicom = pydicom.Dataset()
-
-    # Copie os metadados anonimizados do DICOM original para o DICOM atualizado
-    updated_dicom.update(original_dicom)
-
-    # Atualize outros metadados conforme necessário
-    updated_dicom.PatientName = "Anonymous"
-    updated_dicom.PatientID = "AnonID"
-    updated_dicom.PatientBirthDate = "19000101"
-    updated_dicom.InstitutionName = "Anonymous Institution"
-    updated_dicom.InstitutionAddress = "Anonymous Address"
-    # Adicione mais campos conforme necessário
-
-    # Salve a imagem segmentada no formato DICOM no novo arquivo
-    updated_dicom.PixelData = pixel_array.tobytes()
-    updated_dicom.Rows, updated_dicom.Columns = pixel_array.shape
-    updated_dicom.save_as(save_path.replace('.dcm', '_updated.dcm'))
-
-# ...
+    return segmented_array
 
 @app.route('/segment', methods=['POST'])
 def segment():
-    try:
-        # Verifique se o pedido contém um arquivo DICOM
-        if 'file' not in request.files:
-            return jsonify({'error': 'No DICOM file provided'}), 400
+    if 'file' not in request.files:
+        return jsonify({'error': 'No file part'}), 400
 
-        dicom_file = request.files['file']
+    file = request.files['file']
 
-        # Parâmetros de segmentação definidos pela IA (ajuste conforme necessário)
-        segmentation_parameters = {
-            'lower_threshold': 100,
-            'upper_threshold': 255
-        }
+    if file.filename == '':
+        return jsonify({'error': 'No selected file'}), 400
 
-        # Realize a segmentação
-        processed_dicom_path = segment_image(dicom_file, segmentation_parameters)
+    if file:
+        try:
+            # Segmentar imagem DICOM
+            segmented_array = segment_dicom_image(file)
 
-        # Exiba mensagem de sucesso
-        result_message = (
-            f"Imagem DICOM foi anonimizada e segmentada com sucesso.\n"
-            f"Caminho do arquivo processado: {processed_dicom_path}"
-        )
+            # Salvar imagem segmentada
+            segmented_output_file = "segmented_" + file.filename + ".npy"
+            np.save(segmented_output_file, segmented_array)
 
-        # Retorna o arquivo processado como resposta
-        return send_file(processed_dicom_path), 200
-
-    except Exception as e:
-        return jsonify({'error': str(e)}), 500
-
-# ...
+            return jsonify({'message': 'Imagem DICOM segmentada e salva com sucesso como ' + segmented_output_file})
+        except Exception as e:
+            return jsonify({'error': str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True, threaded=True)
+    app.run(debug=True)
